4c04b13b8b4474f10812f7b0238dc8e8e5fc9e47
EC2 Default User <ec2-user@ip-172-31-18-35.us-east-2.compute.internal>
2019-07-17 18:30:51 +0000

    did the characters creation,and start, and town

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,24 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template('index.html')
+
+@app.route('/character_creation', methods = ['GET','POST'])
+def character_creation():
+    if request.method == 'GET':
+        return "Please enter a name"
+    else:
+        userdata = formopener.dict_from(request.form)
+        vre = userdata['nickname']
+        nickname = vre.decode('utf-8')
+        return render_template('character_creation.html', nickname=nickname)
+
+@app.route('/character_stats', methods = ['GET','POST'])
+def character_stat():
+    if request.method == 'GET':
+        return "Please enter a your type of stat"
+    else:
+        userdata = formopener.dict_from(request.form)
+        vre = userdata['stats']
+        stats = model.stats(vre.decode('utf-8'))
+        return render_template('character_stats.html', stats=stats)
