04adb931b1e4bbfda8ba2003b7db90feba7fd03b
EC2 Default User <ec2-user@ip-172-31-18-35.us-east-2.compute.internal>
2019-07-18 17:10:03 +0000

    finished mvp

--- app/models/model.py
+++ app/models/model.py
@@ -7,8 +7,9 @@ max_health = random.randint(18, 24)
 
 def stats(user_input):
     if user_input.lower() == "fast":
-        return "Your strength: " + str(strength) + "\nYour defense:" + str(defense) + "\nYour speed:" + str(speed) + "\nYour max health:" + str(max_health)
+        return "Your strength: " + str(strength) + "\nYour defense: " + str(defense) + "\nYour speed: " + str(speed) + "\nYour max health: " + str(max_health)
     elif user_input.lower() == "strong":
-        return "Your strength: " + str(strength) + "\nYour defense:" + str(defense) + "\nYour speed:" + str(speed) + "\nYour max health:" + str(max_health)
+        return "Your strength: " + str(strength) + "\nYour defense: " + str(defense) + "\nYour speed: " + str(speed) + "\nYour max health: " + str(max_health)
     elif user_input.lower() == "balance":
-        return "Your strength: " + str(strength) + "\nYour defense:" + str(defense) + "\nYour speed:" + str(speed) + "\nYour max health:" + str(max_health)
+        return "Your strength: " + str(strength) + "\nYour defense: " + str(defense) + "\nYour speed: " + str(speed) + "\nYour max health: " + str(max_health)
+
--- app/routes.py
+++ app/routes.py
@@ -26,3 +26,52 @@ def character_stat():
         vre = userdata['stats']
         stats = model.stats(vre.decode('utf-8'))
         return render_template('character_stats.html', stats=stats)
+        
+@app.route('/town',methods = ['GET','POST'])
+def town():
+    if request.method == 'GET':
+        return "Click on the town"
+    else:
+        userdata = formopener.dict_from(request.form)
+        vre = userdata['Town']
+        town = vre.decode('utf-8')
+        return render_template('town.html', town=town)
+
+@app.route('/enemy',methods = ['GET','POST'])
+def enemy():
+    if request.method == 'GET':
+        return "Click on for enemy"
+    else:
+        userdata = formopener.dict_from(request.form)
+        vre = userdata['enemy']
+        enemy = vre.decode('utf-8')
+        return render_template('enemy.html', enemy=enemy)
+@app.route('/fight',methods = ['GET','POST'])
+def fight():
+    if request.method == 'GET':
+        return "Click on to fight"
+    else:
+        userdata = formopener.dict_from(request.form)
+        vre = userdata['fight']
+        fight = vre.decode('utf-8')
+        return render_template('fight.html', fight=fight)
+
+@app.route('/victory',methods = ['GET','POST'])
+def victory():
+    if request.method == 'GET':
+        return "Click on for a move"
+    else:
+        userdata = formopener.dict_from(request.form)
+        vre = userdata['move']
+        victory = vre.decode('utf-8')
+        return render_template('victory.html', victory=victory)
+
+@app.route('/index',methods = ['GET','POST'])
+def start():
+    if request.method == 'GET':
+        return "Click on to go home"
+    else:
+        userdata = formopener.dict_from(request.form)
+        vre = userdata['start']
+        start = vre.decode('utf-8')
+        return render_template('index.html', start=start)
